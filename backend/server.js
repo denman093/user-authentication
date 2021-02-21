0f5556a7d744ed52b17dd3c0c3d71e2d39b7ba8d
Dennis Pillar <dennis.r.pillar@gmail.com>
2021-02-21 14:43:20 -0600

    initial project commit

---
+++ backend/server.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const dotenv = require('dotenv');
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+dotenv.config();
+
+app.listen(PORT, () => console.log(`Server running on port: ${PORT}`));
+
+mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+},(err) => {
+    if(err)
+        return console.error(err);
+    console.log("Connected to MongoDB");
+})
