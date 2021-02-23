b4c494450b3511a6bcf3202174472fed38f78135
Dennis Pillar <dennis.r.pillar@gmail.com>
2021-02-22 22:26:25 -0600

    router with token/cookie added

--- backend/routers/userRouter.js
+++ backend/routers/userRouter.js
@@ -1,7 +1,10 @@
 const router = require('express').Router();
 const User = require('../models/userModel');
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
-router.post('/', async (req, res) => {
+//register user
+router.post('/register', async (req, res) => {
     try {
         const { email, password, passwordVerify } = req.body;
 
@@ -18,11 +21,41 @@ router.post('/', async (req, res) => {
         const existingUser = await User.findOne({ email });
         if(existingUser)
             return res.status(400).json({ errorMessage: "Email already exists." });
+
+        //hash password
+        const salt = await bcrypt.genSalt();
+        const passwordHash = await bcrypt.hash(password, salt);
+
+        //save new user to db
+        const newUser = new User({
+            email, 
+            passwordHash,
+        });
+
+        const savedUser = await newUser.save();
+
+        //log user in
+        const token = jwt.sign(
+        {
+            user: savedUser._id,
+        },
+            process.env.JWT_SECRET
+        );
+
+        //send the token in an HTTP only cookie
+        res.cookie('token', token, {
+            httpOnly: true,
+        }).send();
+
+        //installed jsonwebtoken
+        //installed bcrypt
     }
     catch(err) {
         console.error(err);
-        res.status(400).send();
+        res.status(500).send();
     }
 });
+
+//log in
 
 module.exports = router;
--- backend/server.js
+++ backend/server.js
@@ -19,4 +19,4 @@ mongoose.connect(process.env.MONGO_URI, {
     console.log("Connected to MongoDB");
 })
 
-app.use('/auth', require('./routers/userRouter'));
+app.use('/identity', require('./routers/userRouter'));
